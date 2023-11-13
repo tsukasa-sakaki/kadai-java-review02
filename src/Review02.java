f60c8598ad50817cd1dfae44ec20f748d72b6e98
tsukasa-sakaki <ts0hg1ck107884y5s@gmail.com>
2023-11-13 22:48:32 +0900

    コメントミスを修正

--- src/Review02.java
+++ src/Review02.java
@@ -2,7 +2,7 @@
 public class Review02 {
 
     public static void main(String[] args) {
-        //iの値が0から100より小さい間、処理を繰り返す
+        //iの値が1から100より小さい間、処理を繰り返す
         for(int i = 1; i <= 100; i++) {
             //iの値が3かつ5で割り切れるとき
             if(i % 3 == 0 && i % 5 == 0 ) {
@@ -12,7 +12,7 @@ public class Review02 {
                System.out.println("Fizz"); //Fizzを表示
             //iの値が5で割り切れるとき
            }else if(i % 5 == 0 ){
-               System.out.println("Buzz"); //buzzを表示
+               System.out.println("Buzz"); //Buzzを表示
             //それ以外の数
            }else{
                System.out.println( + i );
