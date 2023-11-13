83c99360f341aa96aaa0c4b0f3bed60ca3d07c3c
tsukasa-sakaki <ts0hg1ck107884y5s@gmail.com>
2023-11-13 22:37:24 +0900

    Review02

---
+++ src/Review02.java
@@ -0,0 +1,22 @@
+
+public class Review02 {
+
+    public static void main(String[] args) {
+        //iの値が0から100より小さい間、処理を繰り返す
+        for(int i = 1; i <= 100; i++) {
+            //iの値が3かつ5で割り切れるとき
+            if(i % 3 == 0 && i % 5 == 0 ) {
+               System.out.println("FizzBuzz"); //FizzBuzzを表示
+            //iの値が3で割り切れるとき
+           }else if(i % 3 == 0 ){
+               System.out.println("Fizz"); //Fizzを表示
+            //iの値が5で割り切れるとき
+           }else if(i % 5 == 0 ){
+               System.out.println("Buzz"); //buzzを表示
+            //それ以外の数
+           }else{
+               System.out.println( + i );
+           }
+        }
+    }
+}
